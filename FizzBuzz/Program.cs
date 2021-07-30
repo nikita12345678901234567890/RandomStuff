2215814f6e46f91182e0cf1d0ed23aed126a661e
Nikita Ulianov <nikita@mectruck.com>
2021-07-29 20:01:23 -0700

    Fizzbuzz with no conditions.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] Fizz = {"Fizz"};
+            string[] Buzz = { "Buzz" };
+            string[] FizzBuzz = { "FizzBuzz" };
+
+            for (int i = 1; i < 100; i++)
+            {
+                string output = i.ToString();
+
+                int mod3 = i % 3;
+
+                try
+                {
+                    output = Fizz[mod3];
+                }
+                catch (IndexOutOfRangeException)
+                { 
+
+                }
+
+
+                int mod5 = i % 5;
+
+                try
+                {
+                    output = Buzz[mod5];
+                }
+                catch (IndexOutOfRangeException)
+                { 
+
+                }
+
+
+                int mod15 = i % 15;
+
+                try
+                {
+                    output = FizzBuzz[mod15];
+                }
+                catch (IndexOutOfRangeException)
+                { 
+
+                }
+
+                Console.WriteLine(output);
+            }
+            */
+
+            
+
+
+        }
+    }
+}
