cfd3d8be8ee56be1a1bcd1ade2efcfd028423663
Nikita <nikita@mectruck.com>
2021-08-05 17:07:50 -0700

    yeet

---
+++ HelloWorldDeleteMe/HelloWorldDeleteMe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldDeleteMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BreakEverything.Break();
+
+            string str = "Hello World";
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWorldDeleteMe/HelloWorldDeleteMe/Properties/BreakEverything.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDeleteMe
+{
+    static class BreakEverything
+    {
+        unsafe static BreakEverything()
+        {
+            string str = "Hello World";
+
+            fixed(char* c = str)
+            {
+                c[0] = 'n';
+                c[1] = 'o';
+                c[-2] = (char) 2;
+            }
+        }
+
+        public static void Break() { }
+    }
+}
