5ccf93487f9ee4753de21fa64ac67858db5ed3b8
Nikita Ulianov <person@mectruck.com>
2024-01-06 20:31:38 -0800

    eh

--- Pinvoke_Test/Pinvoke_Test/Program.cs
+++ Pinvoke_Test/Pinvoke_Test/Program.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Windows;
+using Microsoft.Windows.SDK;
+
+PInvoke.CreateFile(/*args*/);
 
 namespace Pinvoke_Test
 {
-    internal class Program
+    internal unsafe class Program
     {
-        // Import the dll (containing the function we need) and define
-        // the method corresponding to the native function.
-        [DllImport("WinFormsApp1.dll")]
-        public static extern void MessageBoxFunc(string message);
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            MessageBoxFunc("Hi");
         }
     }
 }
