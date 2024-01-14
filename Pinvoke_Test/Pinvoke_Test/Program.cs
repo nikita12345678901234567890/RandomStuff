4842dc3bdb1ac23ecefbe5cf13e96234a7d0efe7
Nikita Ulianov <person@mectruck.com>
2024-01-13 20:30:35 -0800

    done

--- Pinvoke_Test/Pinvoke_Test/Program.cs
+++ Pinvoke_Test/Pinvoke_Test/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Windows;
-using Microsoft.Windows.SDK;
+using System.Drawing;
 
-PInvoke.CreateFile(/*args*/);
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace Pinvoke_Test
 {
@@ -11,7 +11,34 @@ namespace Pinvoke_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MessageBoxFunc("Hi");
+        }
+
+        public static void MessageBoxFunc(string message)
+        {
+            Point position = new Point(5, 5);
+            Point speed = new Point(10, 10);
+
+            HWND desktopWindow = PInvoke.GetDesktopWindow();
+            WINDOWINFO desktopInfo = default;
+            PInvoke.GetWindowInfo(desktopWindow, ref desktopInfo);
+
+            HWND WindowHandle = PInvoke.GetForegroundWindow();
+            WINDOWINFO windowInfo = default;
+            while (true)
+            {
+                PInvoke.MoveWindow(WindowHandle, position.X, position.Y, 500, 500, false);
+
+                PInvoke.GetWindowInfo(WindowHandle, ref windowInfo);
+                if (windowInfo.rcWindow.top <= desktopInfo.rcWindow.top || windowInfo.rcWindow.bottom >= desktopInfo.rcWindow.bottom) speed.Y *= -1;
+
+                if (windowInfo.rcWindow.left <= desktopInfo.rcWindow.left || windowInfo.rcWindow.right >= desktopInfo.rcWindow.right) speed.X *= -1;
+
+                position.X += speed.X;
+                position.Y += speed.Y;
+
+                Thread.Sleep(1000);
+            }
         }
     }
 }
--- Pinvoke_Test/WinFormsApp1/Form1.Designer.cs
+++ Pinvoke_Test/WinFormsApp1/Form1.Designer.cs
@@ -28,10 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            Name = "Form1";
+            Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.TransparencyKey = System.Drawing.Color.Gold;
+            ClientSize = new Size(111, 148);
+            BackgroundImageLayout = ImageLayout.None;
+            Shown += Form1_Shown;
+            ResumeLayout(false);
         }
 
         #endregion
